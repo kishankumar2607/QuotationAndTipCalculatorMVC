66e29c04d8654e00947164166a06961c35cdbe3c
Kishan Kumar Das <kishankumar2672001@gmail.com>
2025-05-15 20:49:47 -0400

    first commit

---
+++ KishanKumarDas_Assignment_1/Controllers/PriceQuotationController.cs
@@ -0,0 +1,30 @@
+﻿using KishanKumarDas_Assignment_1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KishanKumarDas_Assignment_1.Controllers
+{
+    public class PriceQuotationController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new PriceQuotationModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(PriceQuotationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Clear()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ KishanKumarDas_Assignment_1/Models/PriceQuotationModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KishanKumarDas_Assignment_1.Models
+{
+    public class PriceQuotationModel
+    {
+        [Required(ErrorMessage ="Subtotal is required")]
+        [Range(0, double.MaxValue, ErrorMessage ="Subtotal must be greater than 0.")]
+        public decimal? SubTotal { get; set; }
+
+        [Required(ErrorMessage ="Discount Percentage is required")]
+        [Range(0,100, ErrorMessage ="Discount percentage must be between 0 to 100.")]
+        public decimal? DiscountPercent { get; set; }
+
+        public decimal DiscountAmount =>
+            SubTotal.HasValue && DiscountPercent.HasValue
+                ? SubTotal.Value * DiscountPercent.Value / 100
+                : 0;
+
+        public decimal Total =>
+            SubTotal.HasValue
+                ? SubTotal.Value - DiscountAmount
+                : 0;
+    }
+}
